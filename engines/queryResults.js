89e047dbea719a55cc5e83b0cfbefc4843b5c599
andres lopez rosas <andres22.dev@gmail.com>
2020-02-04 19:32:21 -0600

    correcion

---
+++ engines/queryResults.js
@@ -0,0 +1,65 @@
+const { app } = require('electron');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize
+(
+    {
+        dialect: 'sqlite',
+        storage: __dirname + '\\..\\database\\database.sqlite'
+    }
+);
+
+//cambiar en un futuro para la reutilizacion de codigo
+var Book = sequelize.define('Book', //tabla Book
+{
+    //atributos de Book
+    id:
+    {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false
+    },
+    titulo:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    autor:
+    {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    anio:
+    {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    genero:
+    {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    editorial:
+    {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    contenido:
+    {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    disponible:
+    {
+        type: DataTypes.BOOLEAN
+    }
+
+    //fin de los atributos de Book
+});
+
+
+function getQuery()
+{
+    var results = document.getElementById("results");
+    window.alert("");
+
+}
